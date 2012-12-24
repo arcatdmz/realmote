1a8e837bd62ef88b63216946fb8b8ed275eb2065
Jun Kato <i@junkato.jp>
2012-12-24 09:27:47 +0900

    Added the jar binary.

--- realmote.sh
+++ realmote.sh
@@ -4,4 +4,4 @@
 # -roomba btspp://00066600D69A
 # -fungus /Users/arc/Pictures/
 
-java -Dfile.encoding=UTF-8 -Djava.library.path=./lib/ -classpath ./lib/connector-1.0.4.jar:./lib/bluecove-2.1.1-SNAPSHOT.jar:./lib/RXTXcomm.jar:./lib/phybots-core-1.0.0.jar:./bin jp.digitalmuseum.rm.RealmoteMain $*
+java -Dfile.encoding=UTF-8 -Djava.library.path=./lib/ -classpath ./lib/connector-1.0.4.jar:./lib/bluecove-2.1.1-SNAPSHOT.jar:./lib/RXTXcomm.jar:./lib/phybots-core-1.0.0.jar:./realmote.jar jp.digitalmuseum.rm.RealmoteMain $*
